46c8c0ca30500891d03865f2e9de4c03c0db4b09
Dulat <hupopop@gmail.com>
2021-03-28 22:13:04 +0600

    first commit

---
+++ java/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+<script src=<"scripts.js"></</script>
+
+console.log(4/0);
+console.log('string'*9);
+
+
+let persone = {
+    name: "Jone",
+    age: 25,
+    isMarried: faslse
+}
+
+console.log(persone.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
