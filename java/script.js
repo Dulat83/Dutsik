b07919a56afce7bb8b90f840c1857a0b2d082f1e
Dulat <hupopop@gmail.com>
2021-03-28 22:20:34 +0600

    second

--- java/script.js
+++ java/script.js
@@ -1,28 +1,6 @@
-'use strict';
+<h1>"Hello world</h1>
 
 
-
-var number = 5;
-var string = "Hello";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
-
-<script src=<"scripts.js"></</script>
-
-console.log(4/0);
-console.log('string'*9);
-
-
-let persone = {
-    name: "Jone",
-    age: 25,
-    isMarried: faslse
-}
-
-console.log(persone.name);
 
 
 
